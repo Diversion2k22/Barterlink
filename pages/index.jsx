f8b8aa81b0cdbb7df9f631914a288ded08434e57
chaitak-gorai <chaitakgorai@gmail.com>
2022-02-26 22:54:57 +0530

    ⚡️ Metamask Connection

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,12 +2,48 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import { useWeb3 } from '@3rdweb/hooks'
+import { useEffect } from 'react'
+import { client } from '../lib/sanityClient'
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
 const Home = () => {
+  const { address, connectWallet } = useWeb3()
+  useEffect(() => {
+    if (!address) return
+    ;(async () => {
+      const userDoc = {
+        _type: 'users',
+        _id: address,
+        useName: 'Unnamed',
+        walletAddress: address,
+      }
+      const result = await client.createIfNotExists(userDoc)
+    })()
+  }, [address])
   return (
-    <>
-      <Header />
-      <Hero />
-    </>
+    <div className={style.wrapper}>
+      {address ? (
+        <>
+          <Header />
+          <Hero />
+        </>
+      ) : (
+        <div className={style.walletConnectWrapper}>
+          <button
+            className={style.button}
+            onClick={() => connectWallet('injected')}
+          >
+            Connection Walled
+          </button>
+          <div className={style.details}>You need Metamask</div>
+        </div>
+      )}
+    </div>
   )
 }
 
